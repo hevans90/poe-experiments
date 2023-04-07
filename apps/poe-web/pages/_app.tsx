ec995d3db53704082efe732f6072a6cb447393c0
hevans90 <hevans9000@gmail.com>
2023-04-07 10:17:40 +0100

    feat(nhost): poc

--- apps/poe-web/pages/_app.tsx
+++ apps/poe-web/pages/_app.tsx
@@ -1,13 +1,23 @@
+import { NhostClient, NhostNextProvider } from '@nhost/nextjs';
 import { AppProps } from 'next/app';
+
+import { UserProvider } from '../user-provider';
 import './styles.css';
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || '',
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <UserProvider>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </UserProvider>
+    </NhostNextProvider>
   );
 }
 
